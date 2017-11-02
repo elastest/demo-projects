b1c6a043e5b070e0fec15dd1cef1e88df4df4e80
Micael Gallego <micael.gallego@gmail.com>
2017-11-02 23:31:44 +0100

    Add OpenVidu e2e Tests

---
+++ openvidu-test/docker/create_image.sh
@@ -0,0 +1,23 @@
+# openvidu-server
+git clone https://github.com/OpenVidu/openvidu.git
+cd openvidu
+mvn -DskipTests=true install
+mvn -DskipTests=true compile
+cd openvidu-server
+mvn clean compile package
+cp ./target/openvidu-server-1.1.1.jar ../../openvidu-server.jar
+
+# openvidu-testapp
+cd ../openvidu-testapp
+npm install
+ng build
+cp -r ./dist ../../openvidu-testapp
+
+# Build docker image
+cd ../../
+docker build -t openvidu/openvidu-elastest-demo .
+
+# Delete unwanted files
+rm -rf ./openvidu-testapp
+rm openvidu-server.jar
+rm -rf ./openvidu
