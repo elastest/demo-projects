9fa7ab58d468001f0d86e980f013b1c9f3fd101d
Micael Gallego <micael.gallego@gmail.com>
2017-11-02 23:56:48 +0100

    Allow browser type selection in web-java-test

--- web-java-test/src/test/java/io/elastest/demo/web/WebAppTest.java
+++ web-java-test/src/test/java/io/elastest/demo/web/WebAppTest.java
@@ -27,75 +27,102 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
 
 public class WebAppTest {
 
-    private WebDriver driver;
-    private static String eusApiURL;
-    private static String appURL;
+	public static final String CHROME = "chrome";
+	public static final String FIREFOX = "firefox";
 
-    @BeforeAll
-    public static void setupClass() {
-        eusApiURL = System.getenv("ET_EUS_API");
-        if (eusApiURL == null) {
-            ChromeDriverManager.getInstance().setup();
-        }
+	private static String browserType;
+	private static String eusApiURL;
+	private static String appURL;
+	
+	private WebDriver driver;	
+	
+	@BeforeAll
+	public static void setupClass() {
 
-        appURL = System.getenv("APP_IP");
-        if (appURL == null) {
-            appURL = "http://localhost:8080/";
-        }
-        System.out.println("App url: " + appURL);
-    }
+		browserType = System.getenv("browser");
+		
+		eusApiURL = System.getenv("ET_EUS_API");
+		if (eusApiURL == null) {
 
-    @BeforeEach
-    public void setupTest() throws MalformedURLException {
-        if (eusApiURL == null) {
-            driver = new ChromeDriver();
-        } else {
-            driver = new RemoteWebDriver(new URL(eusApiURL),
-                    DesiredCapabilities.chrome());
-        }
-    }
+			if (browserType == null || browserType.equals(CHROME)) {
+				ChromeDriverManager.getInstance().setup();
+			} else {
+				FirefoxDriverManager.getInstance().setup();
+			}
+		}
 
-    @AfterEach
-    public void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+		appURL = System.getenv("APP_IP");
+		if (appURL == null) {
+			appURL = "http://localhost:8080/";
+		}
+		System.out.println("App url: " + appURL);
+	}
 
-    @Test
-    public void test() throws InterruptedException {
-        driver.get(appURL);
+	@BeforeEach
+	public void setupTest() throws MalformedURLException {
+		if (eusApiURL == null) {
+			if (browserType == null || browserType.equals(CHROME)) {
+				driver = new ChromeDriver();
+			} else {
+				driver = new FirefoxDriver();
+			}
+		} else {
+			
+			Capabilities caps;
+			if (browserType == null || browserType.equals(CHROME)) {
+				caps = DesiredCapabilities.chrome();
+			} else {
+				caps = DesiredCapabilities.firefox();
+			}
+			
+			driver = new RemoteWebDriver(new URL(eusApiURL), caps);
+		}
+	}
 
-        Thread.sleep(3000);
+	@AfterEach
+	public void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 
-        String newTitle = "MessageTitle";
-        String newBody = "MessageBody";
+	@Test
+	public void test() throws InterruptedException {
+		driver.get(appURL);
 
-        driver.findElement(By.id("title-input")).sendKeys(newTitle);
-        driver.findElement(By.id("body-input")).sendKeys(newBody);
+		Thread.sleep(3000);
 
-        Thread.sleep(3000);
+		String newTitle = "MessageTitle";
+		String newBody = "MessageBody";
 
-        driver.findElement(By.id("submit")).click();
+		driver.findElement(By.id("title-input")).sendKeys(newTitle);
+		driver.findElement(By.id("body-input")).sendKeys(newBody);
 
-        Thread.sleep(3000);
+		Thread.sleep(3000);
 
-        String title = driver.findElement(By.id("title")).getText();
-        String body = driver.findElement(By.id("body")).getText();
+		driver.findElement(By.id("submit")).click();
 
-        assertThat(title, equalTo(newTitle));
-        assertThat(body, equalTo(newBody));
+		Thread.sleep(3000);
 
-        Thread.sleep(3000);
-    }
+		String title = driver.findElement(By.id("title")).getText();
+		String body = driver.findElement(By.id("body")).getText();
+
+		assertThat(title, equalTo(newTitle));
+		assertThat(body, equalTo(newBody));
+
+		Thread.sleep(3000);
+	}
 
 }
