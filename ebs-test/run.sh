f35f6073415bce9080be4c20c544baee1c5c9e7c
Nikos Gavalas <ns.gavalas@gmail.com>
2018-01-08 17:45:43 +0200

    scripted execution

---
+++ ebs-test/run.sh
@@ -0,0 +1,19 @@
+git clone https://github.com/LordWire/demo-projects.git
+
+#cd to the directory
+cd demo-projects/ebs-test
+
+#this works too
+mvn package
+
+#get the big file
+wget https://norvig.com/big.txt
+
+
+#clean the pre-existing file
+hadoop fs  -rmr /out.txt
+
+spark-submit --class org.sparkexample.WordCountTask --master spark://spark-master:7077 /demo-projects/ebs-test/target/hadoopWordCount-1.0-SNAPSHOT.jar /demo-projects/ebs-test/big.txt
+
+hadoop fs -getmerge /out.txt ./out.txt
+head -10 out.txt
