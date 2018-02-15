127bc4c2dcf230e299167e75ab7e0774ce1dce16
EduJGURJC <ejg.urjc@gmail.com>
2018-02-15 12:30:53 +0100

    WebApp => print application messages

--- web-java-test/src/main/java/io/elastest/demo/web/MessageController.java
+++ web-java-test/src/main/java/io/elastest/demo/web/MessageController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MessageController {
+    private static final Logger logger = LoggerFactory
+            .getLogger(MessageController.class);
 
     private List<Message> messages = Collections
             .synchronizedList(new ArrayList<>());
@@ -27,7 +31,7 @@ public class MessageController {
     public String newMessage(Message message) {
 
         messages.add(message);
-
+        logger.info("Message added succesfully!");
         return "redirect:/";
     }
 
@@ -35,7 +39,7 @@ public class MessageController {
     public String clearMessages() {
 
         this.messages = Collections.synchronizedList(new ArrayList<>());
-
+        logger.info("All messages have been cleaned succesufully!");
         return "redirect:/";
     }
 
