9f5b87a9992b83c32f3679547d163241eec13463
franciscoRdiaz <francisco.diazbayon@gmail.com>
2018-05-22 11:31:09 +0200

    Add new test to see if old surefire reports are removed

---
+++ simple-web-java-test/src/test/java/io/elastest/demo/web/WebAppTest.java
@@ -0,0 +1,158 @@
+/*
+ * (C) Copyright 2017-2019 ElasTest (http://elastest.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.elastest.demo.web;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+
+public class WebAppTest {
+    private static final Logger logger = LoggerFactory
+            .getLogger(WebAppTest.class);
+
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+
+    private static String browserType;
+    private static String eusURL;
+    private static String sutUrl;
+
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupClass() {
+
+        browserType = System.getProperty("browser");
+
+        System.out.println("Browser Type: " + browserType);
+
+        eusURL = System.getenv("ET_EUS_API");
+        if (eusURL == null) {
+
+            if (browserType == null || browserType.equals(CHROME)) {
+                ChromeDriverManager.getInstance().setup();
+            } else {
+                FirefoxDriverManager.getInstance().setup();
+            }
+        }
+
+        String sutHost = System.getenv("ET_SUT_HOST");
+        String sutPort = System.getProperty("etSutPort");
+        if (sutHost == null) {
+            sutUrl = "http://localhost:8080/";
+        } else {
+            sutUrl = "http://" + sutHost + (sutPort != null ? (":" + sutPort + "/"): ":8080/");
+        }
+        System.out.println("Webapp URL: " + sutUrl);
+    }
+
+    // @BeforeEach
+    public void setupTest(String testName) throws MalformedURLException {
+        if (eusURL == null) {
+            if (browserType == null || browserType.equals(CHROME)) {
+                driver = new ChromeDriver();
+            } else {
+                driver = new FirefoxDriver();
+            }
+        } else {
+            DesiredCapabilities caps;
+            if (browserType == null || browserType.equals(CHROME)) {
+                caps = DesiredCapabilities.chrome();
+            } else {
+                caps = DesiredCapabilities.firefox();
+            }
+            caps.setCapability("browserId", testName);
+
+            driver = new RemoteWebDriver(new URL(eusURL), caps);
+        }
+    }
+
+    @AfterEach
+    public void teardown(TestInfo info) {
+        String testName = info.getDisplayName();
+        testName = testName.replaceAll("\\(", "").replaceAll("\\)", "");
+
+        if (driver != null) {
+            logger.info("Clearing Messages...");
+            driver.findElement(By.id("clearSubmit")).click();
+
+            logger.info("##### Finish test: {}", testName);
+
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() throws InterruptedException, MalformedURLException {
+        String testName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+
+        logger.info("##### Start test: {}", testName);
+        this.setupTest(testName);
+
+        driver.get(sutUrl);
+        driver.manage().addCookie(new Cookie("sss", "{\"elastest\"}"));
+        ((JavascriptExecutor) driver)
+                .executeScript("console.log('Hola caracola')");
+
+        Thread.sleep(2000);
+
+        String newTitle = "MessageTitle";
+        String newBody = "MessageBody";
+
+        driver.findElement(By.id("title-input")).sendKeys(newTitle);
+        driver.findElement(By.id("body-input")).sendKeys(newBody);
+
+        Thread.sleep(2000);
+
+        logger.info("Adding Message...");
+        driver.findElement(By.id("submit")).click();
+
+        Thread.sleep(2000);
+
+        String title = driver.findElement(By.id("title")).getText();
+        String body = driver.findElement(By.id("body")).getText();
+
+        logger.info("Checking Message...");
+        assertThat(title, equalTo(newTitle));
+        assertThat(body, equalTo(newBody));
+
+        Thread.sleep(2500);
+
+    }
+
+}
