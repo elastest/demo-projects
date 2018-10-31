4ec22cba47b4023b962fb9bf889eecb930a8a7ae
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-10-31 14:33:20 +0100

    protractor improvements

--- protractor-webapp/conf.js
+++ protractor-webapp/conf.js
@@ -1,16 +1,11 @@
+var env = require('./envs.js');
+
 exports.config = {
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+    seleniumAddress: env.seleniumAddress,
     specs: ['test-webapp-spec.js'],
     sutUrl: 'http://172.17.0.2:8080',
+    capabilities: env.capabilities,
     onPrepare: function() {
-        console.log(JSON.stringify(process.env));
-        if (typeof process.env.ET_EUS_API !== 'undefined') {
-            this.seleniumAddress = process.env.ET_EUS_API;
-        }
-        if (typeof process.env.ET_SUT_HOST !== 'undefined') {
-            this.sutUrl = process.env.ET_SUT_HOST;
-        }
-
         var jasmineReporters = require('jasmine-reporters');
         jasmine.getEnv().addReporter(
             new jasmineReporters.JUnitXmlReporter({
---
+++ protractor-webapp/envs.js
@@ -0,0 +1,13 @@
+console.log(JSON.stringify(process.env));
+
+module.exports = {
+    // The address of a running selenium server.
+    seleniumAddress: process.env.ET_EUS_API || 'http://localhost:4444/wd/hub',
+    sutUrl: process.env.ET_SUT_HOST || 'http://172.17.0.2:8080',
+
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        browserName: process.env.BROWSER || 'chrome',
+        version: process.env.BROWSER_VERSION || 'latest',
+    },
+};
