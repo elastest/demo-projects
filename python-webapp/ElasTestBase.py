2eaf5459baa916ff5f1dbfb072f64d77cf0e4a8b
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-11-06 14:51:16 +0100

    Refactor python test => use General ElasTestBase.py file

---
+++ python-webapp/ElasTestBase.py
@@ -0,0 +1,43 @@
+import unittest
+import os
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+driver = None
+
+
+class ElasTestBase(unittest.TestCase):
+    def setUp(self):
+        global driver
+
+        testName = self._testMethodName
+        print '##### Start test: ' + testName
+
+        if('ET_EUS_API' in os.environ):
+            capabilities = DesiredCapabilities.CHROME
+            if('BROWSER' in os.environ):
+                if(os.environ['BROWSER'] == 'firefox'):
+                    capabilities = DesiredCapabilities.FIREFOX
+
+            capabilities['browserId'] = testName
+
+            driver = webdriver.Remote(
+                command_executor=os.environ['ET_EUS_API'],
+                desired_capabilities=capabilities)
+        else:
+            driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+
+        sutIp = '172.17.0.3'
+        if('ET_SUT_HOST' in os.environ):
+            sutIp = os.environ['ET_SUT_HOST']
+
+        sutUrl = 'http://' + sutIp + ':8080'
+        driver.get(sutUrl)
+        return driver
+
+    def tearDown(self):
+        global driver
+        testName = self._testMethodName
+        print '##### Finish test: ' + testName
+        driver.close()
--- python-webapp/WebappTest.py
+++ python-webapp/WebappTest.py
@@ -1,14 +1,12 @@
 import unittest
 import os
 import sys
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 import xmlrunner
+import ElasTestBase
 
 
 def getElementById(driver, id, timeout=10):
@@ -23,74 +21,46 @@ def addRow(driver, title, body):
     getElementById(driver, 'submit').click()
 
 
-driver = None
+def clearData(driver):
+    print 'Clearing Messages...'
+    getElementById(driver, 'clearSubmit').click()
 
 
-class TestWebApp(unittest.TestCase):
-    def setUp(self):
-        global driver
-
-        testName = self._testMethodName
-        print '##### Start test: ' + testName
-
-        if('ET_EUS_API' in os.environ):
-            capabilities = DesiredCapabilities.CHROME
-            if('BROWSER' in os.environ):
-                if(os.environ['BROWSER'] == 'firefox'):
-                    capabilities = DesiredCapabilities.FIREFOX
-
-            capabilities['browserId'] = testName
-
-            driver = webdriver.Remote(
-                command_executor=os.environ['ET_EUS_API'],
-                desired_capabilities=capabilities)
-        else:
-            driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
-
-        sutIp = '172.17.0.3'
-        if('ET_SUT_HOST' in os.environ):
-            sutIp = os.environ['ET_SUT_HOST']
-
-        sutUrl = 'http://' + sutIp + ':8080'
-        driver.get(sutUrl)
-        return driver
-
-    def tearDown(self):
-        global driver
-        print 'Clearing Messages...'
-        getElementById(driver, 'clearSubmit').click()
-
-        testName = self._testMethodName
-        print '##### Finish test: ' + testName
-        driver.close()
-
-    # ############# #
-    # ### TESTS ### #
-    # ############# #
+class TestWebApp(ElasTestBase.ElasTestBase):
     def test_check_title_and_body_not_empty(self):
-        global driver
-        addRow(driver, '', '')
-        time.sleep(2)
+        driver = ElasTestBase.driver
+        try:
+            addRow(driver, '', '')
+            time.sleep(2)
 
-        title = getElementById(driver, 'title').text
-        body = getElementById(driver, 'body').text
-        print 'Checking Message...'
+            title = getElementById(driver, 'title').text
+            body = getElementById(driver, 'body').text
+            print 'Checking Message...'
 
-        self.assertNotEqual('', title)
-        self.assertNotEqual('', body)
+            self.assertNotEqual('', title)
+            self.assertNotEqual('', body)
+        except Exception as e:
+            clearData(driver)
+            sys.exit(1)
+        clearData(driver)
+
 
     def test_find_title_and_body(self):
-        global driver
-        addRow(driver, 'MessageTitle', 'MessageBody')
-        time.sleep(2)
+        driver = ElasTestBase.driver
+        try:
+            addRow(driver, 'MessageTitle', 'MessageBody')
+            time.sleep(2)
 
-        title = getElementById(driver, 'title').text
-        body = getElementById(driver, 'body').text
-        print 'Checking Message...'
+            title = getElementById(driver, 'title').text
+            body = getElementById(driver, 'body').text
+            print 'Checking Message...'
 
-        self.assertEqual('MessageTitle', title)
-        self.assertEqual('MessageBody', body)
-
+            self.assertEqual('MessageTitle', title)
+            self.assertEqual('MessageBody', body)
+        except Exception as e:
+            clearData(driver)
+            sys.exit(1)
+        clearData(driver)
 
 if __name__ == '__main__':
     file_path = './testresults'
