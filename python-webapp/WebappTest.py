caed2278b730bcafe5e5b3c5c70b2f7d6b7215c9
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-11-06 12:40:20 +0100

    Fix python test

--- python-webapp/WebappTest.py
+++ python-webapp/WebappTest.py
@@ -86,7 +86,11 @@ class TestWebApp(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    with open('./testresults/results.xml', 'wb') as output:
+    file_path = './testresults'
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    file_name = file_path + '/results.xml'
+    with open(file_name, 'wb') as output:
         unittest.main(
             testRunner=xmlrunner.XMLTestRunner(output=output),
             failfast=False, buffer=False, catchbreak=False)
