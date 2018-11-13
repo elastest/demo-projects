1cb9abc0c78ee804f01adc073ddbf818e86f575a
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-11-13 08:44:51 +0100

    Refactor Jasmine unit test

---
+++ jasmine-unit-test/calc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    sum: function(a, b) {
+        return Number(a) + Number(b);
+    },
+
+    sub: function(a, b) {
+        return Number(a) - Number(b);
+    },
+};
--- jasmine-unit-test/spec/unit-test-spec.js
+++ jasmine-unit-test/spec/unit-test-spec.js
@@ -1,4 +1,5 @@
 require('./elastest-conf.js');
+var calc = require('../calc.js');
 
 leftOperand = 3;
 rightOperand = 2;
@@ -7,20 +8,12 @@ describe('Unit Test', function() {
     it('Sum', function() {
         expectedResult = 5;
         console.log('Checking if ' + leftOperand + ' + ' + rightOperand + ' = ' + expectedResult);
-        expect(sum(leftOperand, rightOperand)).toEqual(expectedResult);
+        expect(calc.sum(leftOperand, rightOperand)).toEqual(expectedResult);
     });
 
     it('Sub', function() {
         expectedResult = 1;
         console.log('Checking if ' + leftOperand + ' - ' + rightOperand + ' = ' + expectedResult);
-        expect(sub(leftOperand, rightOperand)).toEqual(expectedResult);
+        expect(calc.sub(leftOperand, rightOperand)).toEqual(expectedResult);
     });
 });
-
-function sum(a, b) {
-    return Number(a) + Number(b);
-}
-
-function sub(a, b) {
-    return Number(a) - Number(b);
-}
