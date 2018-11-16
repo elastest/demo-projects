55d87e2117d455eee71c30cc5b3d4fdc43d2cc41
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-11-16 10:30:07 +0100

    remove old projects

--- unit-java-test/python-unit-test/Calc.py
+++
@@ -1,6 +0,0 @@
-def sum(a, b):
-    return int(a) + int(b)
-
-
-def sub(a, b):
-    return int(a) - int(b)
--- unit-java-test/python-unit-test/ElasTestBase.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class ElasTestBase(unittest.TestCase):
-    def setUp(self):
-        print '##### Start test: ' + self._testMethodName
-
-    def tearDown(self):
-        print '##### Finish test: ' + self._testMethodName
