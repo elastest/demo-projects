976eebb826bbec62d005d49c41e0a51927c86809
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-11-16 13:57:56 +0100

    fix

--- webapp/cucumber-web-multiple-browsers-test/src/test/java/io/elastest/demo/web/WebAppTestRunner.java
+++ webapp/cucumber-web-multiple-browsers-test/src/test/java/io/elastest/demo/web/WebAppTestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/multiple-webapp-tests.feature", plugin = {
+@CucumberOptions(features = "src/test/resources/webapp-test.feature", plugin = {
         "html:target/surefire-reports/cucumber-html-report",
         "json:target/surefire-reports/cucumber.json",
         "pretty" }, glue = { "io.elastest.demo.web.multiple" })
