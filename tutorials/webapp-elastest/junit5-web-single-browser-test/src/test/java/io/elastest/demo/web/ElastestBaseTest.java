e2b06fbefc060b240ffd4284846932329c17a48e
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-12-18 12:56:07 +0100

    copy junit5 simple webapp to tutorial and add screenshots

---
+++ tutorials/webapp-elastest/junit5-web-single-browser-test/src/test/java/io/elastest/demo/web/ElastestBaseTest.java
@@ -0,0 +1,127 @@
+package io.elastest.demo.web;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import static org.openqa.selenium.OutputType.BASE64;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ElastestBaseTest {
+    protected static final Logger logger = LoggerFactory
+            .getLogger(ElastestBaseTest.class);
+
+    protected static final String CHROME = "chrome";
+    protected static final String FIREFOX = "firefox";
+
+    protected static String browserType;
+    protected static String browserVersion;
+    protected static String eusURL;
+    protected static String sutUrl;
+
+    protected static WebDriver driver;
+    protected static String sutLogsPath;
+
+    @BeforeAll
+    public static void setupClass() throws MalformedURLException {
+        // If first time, init
+        if (driver == null) {
+            String sutHost = System.getenv("ET_SUT_HOST");
+            String sutPort = System.getenv("ET_SUT_PORT");
+            String sutProtocol = System.getenv("ET_SUT_PROTOCOL");
+            sutLogsPath = System.getenv("ET_SUT_LOGS_PATH");
+
+            if (sutHost == null) {
+                sutUrl = "http://localhost:8080/";
+            } else {
+                sutPort = sutPort != null ? sutPort : "8080";
+                sutProtocol = sutProtocol != null ? sutProtocol : "http";
+
+                sutUrl = sutProtocol + "://" + sutHost + ":" + sutPort;
+            }
+            logger.info("Webapp URL: " + sutUrl);
+
+            browserType = System.getProperty("browser");
+            logger.info("Browser Type: {}", browserType);
+            eusURL = System.getenv("ET_EUS_API");
+
+            if (eusURL == null) {
+                if (browserType == null || browserType.equals(CHROME)) {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                } else {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+            } else {
+                DesiredCapabilities caps;
+                if (browserType == null || browserType.equals(CHROME)) {
+                    caps = DesiredCapabilities.chrome();
+                } else {
+                    caps = DesiredCapabilities.firefox();
+                }
+
+                browserVersion = System.getProperty("browserVersion");
+                if (browserVersion != null) {
+                    logger.info("Browser Version: {}", browserVersion);
+                    caps.setVersion(browserVersion);
+                }
+                driver = new RemoteWebDriver(new URL(eusURL), caps);
+            }
+
+            // driver quit when all tests end
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                public void run() {
+                    logger.info("Shutting down browser...");
+                    driver.quit();
+                }
+            });
+        }
+    }
+
+    @BeforeEach
+    public void setupTest(TestInfo info) throws MalformedURLException {
+        String testName = info.getTestMethod().get().getName();
+        ((JavascriptExecutor) driver).executeScript(
+                "'{\"elastestCommand\": \"startTest\", \"args\": {\"testName\": \""
+                        + testName + "\"} }'");
+
+        logger.info("##### Start test: {}", testName);
+
+        driver.get(sutUrl);
+    }
+
+    @AfterEach
+    public void teardown(TestInfo info) {
+        String testName = info.getTestMethod().get().getName();
+        logger.info("##### Finish test: {}", testName);
+    }
+
+    void logBase64Screenshot(WebDriver driver, String fileName) {
+        if (fileName != null) {
+            try {
+                String screenshotBase64 = ((TakesScreenshot) driver)
+                        .getScreenshotAs(BASE64);
+                logger.info("Screenshot (in Base64) at the end of {} "
+                        + "(copy&paste this string as URL in browser to watch it):\r\n"
+                        + "data:image/png;base64,{}", fileName,
+                        screenshotBase64);
+            } catch (Exception e) {
+                logger.trace("Exception getting screenshot in Base64", e);
+            }
+        }
+    }
+}
