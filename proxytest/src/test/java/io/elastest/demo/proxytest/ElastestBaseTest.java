2f17f74437a401d4e077be45d34f39d6f1b28394
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2019-02-04 15:41:17 +0100

    fix

--- proxytest/src/test/java/io/elastest/demo/proxytest/ElastestBaseTest.java
+++ proxytest/src/test/java/io/elastest/demo/proxytest/ElastestBaseTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -31,90 +30,88 @@ public class ElastestBaseTest {
     protected static String eusURL;
     protected static String sutUrl;
 
-    protected static WebDriver driver;
+    protected WebDriver driver;
 
     @BeforeAll
-    public static void setupClass() throws MalformedURLException {
-        // If first time, init
-        if (driver == null) {
-            String sutHost = System.getenv("ET_SUT_HOST");
-            String sutPort = System.getenv("ET_SUT_PORT");
-            String sutProtocol = System.getenv("ET_SUT_PROTOCOL");
+    public static void setupClass() {
+        String sutHost = System.getenv("ET_SUT_HOST");
+        String sutPort = System.getenv("ET_SUT_PORT");
+        String sutProtocol = System.getenv("ET_SUT_PROTOCOL");
 
-            String proxyUrl = System.getenv("PROXY_URL");
+        if (sutHost == null) {
+            sutUrl = "https://elastest.io/docs/";
+        } else {
+            sutProtocol = sutProtocol != null ? sutProtocol : "http";
 
-            if (sutHost == null) {
-                sutUrl = "https://elastest.io/docs/";
+            sutUrl = sutProtocol + "://" + sutHost
+                    + (sutPort != null ? ":" + sutPort : "");
+        }
+        
+        logger.info("Webapp URL: " + sutUrl);
+
+        browserType = System.getProperty("browser");
+        logger.info("Browser Type: {}", browserType);
+        eusURL = System.getenv("ET_EUS_API");
+        
+        if (eusURL == null) {
+            if (browserType == null || browserType.equals(CHROME)) {
+                WebDriverManager.chromedriver().setup();
             } else {
-                sutProtocol = sutProtocol != null ? sutProtocol : "http";
-
-                sutUrl = sutProtocol + "://" + sutHost
-                        + (sutPort != null ? ":" + sutPort : "");
+                WebDriverManager.firefoxdriver().setup();
             }
-            logger.info("Webapp URL: " + sutUrl);
-
-            browserType = System.getenv("browser");
-            logger.info("Browser Type: {}", browserType);
-            eusURL = System.getenv("ET_EUS_API");
-
-            if (eusURL == null) {
-                if (browserType == null || browserType.equals(CHROME)) {
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                } else {
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                }
-            } else {
-                logger.info("Using ElasTest EUS URL: {}", eusURL);
-
-                DesiredCapabilities caps;
-                if (browserType == null || browserType.equals(CHROME)) {
-                    caps = DesiredCapabilities.chrome();
-                } else {
-                    caps = DesiredCapabilities.firefox();
-                }
-
-                if (proxyUrl != null) {
-                    Proxy proxy = new Proxy();
-                    proxy.setHttpProxy(proxyUrl);
-                    logger.info("Using proxy url: {}", proxyUrl);
-                    caps.setCapability("proxy", proxy);
-                }
-
-                browserVersion = System.getenv("browserVersion");
-                if (browserVersion != null) {
-                    logger.info("Browser Version: {}", browserVersion);
-                    caps.setVersion(browserVersion);
-                }
-                driver = new RemoteWebDriver(new URL(eusURL), caps);
-            }
-
-            // driver quit when all tests end
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                public void run() {
-                    logger.info("Shutting down browser...");
-                    driver.quit();
-                }
-            });
         }
     }
 
     @BeforeEach
     public void setupTest(TestInfo info) throws MalformedURLException {
         String testName = info.getTestMethod().get().getName();
-        ((JavascriptExecutor) driver).executeScript(
-                "'{\"elastestCommand\": \"startTest\", \"args\": {\"testName\": \""
-                        + testName + "\"} }'");
-
         logger.info("##### Start test: {}", testName);
+
+        if (eusURL == null) {
+            if (browserType == null || browserType.equals(CHROME)) {
+                driver = new ChromeDriver();
+            } else {
+                driver = new FirefoxDriver();
+            }
+        } else {
+            logger.info("Using ElasTest EUS URL: {}", eusURL);
+
+            DesiredCapabilities caps;
+            if (browserType == null || browserType.equals(CHROME)) {
+                caps = DesiredCapabilities.chrome();
+            } else {
+                caps = DesiredCapabilities.firefox();
+            }
+            
+            String proxyUrl = System.getenv("PROXY_URL");
+            if (proxyUrl != null) {
+                Proxy proxy = new Proxy();
+                proxy.setHttpProxy(proxyUrl);
+                logger.info("Using proxy url: {}", proxyUrl);
+                caps.setCapability("proxy", proxy);
+            }
+
+            browserVersion = System.getProperty("browserVersion");
+            if (browserVersion != null) {
+                logger.info("Browser Version: {}", browserVersion);
+                caps.setVersion(browserVersion);
+            }
+
+            caps.setCapability("testName", testName);
+            driver = new RemoteWebDriver(new URL(eusURL), caps);
+        }
 
         driver.get(sutUrl);
     }
 
     @AfterEach
     public void teardown(TestInfo info) {
+        if (driver != null) {
+            driver.quit();
+        }
+
         String testName = info.getTestMethod().get().getName();
         logger.info("##### Finish test: {}", testName);
     }
+
 }
