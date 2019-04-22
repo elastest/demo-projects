ac17b57e10c4d67bc539d33b6acc22282e12bf28
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2019-04-22 09:10:45 +0200

    Update Junit5 test => allow to pass operands and expected results by env

--- unit/junit5-unit-test/src/test/java/io/elastest/demo/unit/CalcTest.java
+++ unit/junit5-unit-test/src/test/java/io/elastest/demo/unit/CalcTest.java
@@ -27,25 +27,56 @@ public class CalcTest extends ElasTestBase {
 
     static int left = 3;
     static int right = 2;
+    static int expectedSumResult = 5;
+    static int expectedSubResult = 1;
+
+    static final String LEFT_KEY = "LEFT";
+    static final String RIGHT_KEY = "RIGHT";
+    static final String EXPECTED_SUM_RESULT_KEY = "EXPECTED_SUM_RESULT";
+    static final String EXPECTED_SUB_RESULT_KEY = "EXPECTED_SUB_RESULT";
 
     @BeforeEach
     public void setup() {
         this.calc = new Calc();
+
+        if (System.getenv(LEFT_KEY) != null
+                && !"".equals(System.getenv(LEFT_KEY))
+
+                && System.getenv(RIGHT_KEY) != null
+                && !"".equals(System.getenv(RIGHT_KEY))
+
+                && System.getenv(EXPECTED_SUM_RESULT_KEY) != null
+                && !"".equals(System.getenv(EXPECTED_SUM_RESULT_KEY))
+
+                && System.getenv(EXPECTED_SUB_RESULT_KEY) != null
+                && !"".equals(System.getenv(EXPECTED_SUB_RESULT_KEY))) {
+            left = Integer.parseInt(System.getenv(LEFT_KEY));
+            right = Integer.parseInt(System.getenv(RIGHT_KEY));
+
+            expectedSumResult = Integer
+                    .parseInt(System.getenv(EXPECTED_SUM_RESULT_KEY));
+            expectedSubResult = Integer
+                    .parseInt(System.getenv(EXPECTED_SUB_RESULT_KEY));
+
+            logger.info("Using passed values:");
+            logger.info("left: {}", left);
+            logger.info("right: {}", right);
+            logger.info("expected Sum result: {}", expectedSumResult);
+            logger.info("expected Sub result: {}", expectedSubResult);
+        } else {
+            logger.info("Using default values:");
+        }
     }
 
     @Test
     public void sumTest() {
-        int expectedResult = 5;
-        logger.info("Checking if {} + {} = {}", left, right, expectedResult);
-        assertEquals(expectedResult, calc.sum(left, right));
+        logger.info("Checking if {} + {} = {}", left, right, expectedSumResult);
+        assertEquals(expectedSumResult, calc.sum(left, right));
     }
 
     @Test
     public void subTest() {
-
-        int expectedResult = 1;
-        logger.info("Checking if {} + {} = {}", left, right, expectedResult);
-        assertEquals(expectedResult, calc.sub(left, right));
+        logger.info("Checking if {} + {} = {}", left, right, expectedSubResult);
+        assertEquals(expectedSubResult, calc.sub(left, right));
     }
 }
-
