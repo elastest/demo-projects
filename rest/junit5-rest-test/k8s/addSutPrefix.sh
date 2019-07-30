e6b90e3b875e1ac6ef09e24c2da89dd102144a97
franciscoRdiaz <francisco.diazbayon@gmail.com>
2019-07-30 12:48:53 +0200

    Update label value

--- rest/junit5-rest-test/k8s/addSutPrefix.sh
+++ rest/junit5-rest-test/k8s/addSutPrefix.sh
@@ -1,3 +1,4 @@
 #/!bin/bash
 # Add sut prefix for the current execution 
 sed -i -e "s/name: demo-rest-java-sut/name: ${ET_SUT_CONTAINER_NAME}-demo-rest-java-sut/g" -e 's/sut_/sut-/g' rest-app-deployment.yaml
+sed -i -e "s/io.elastest.service: demo-rest-java-sut/io.elastest.service: ${ET_SUT_CONTAINER_NAME}-demo-rest-java-sut/g" rest-app-deployment.yaml
