338c026560a0ea7d5cdc498f0061f715bf574a53
franciscoRdiaz <francisco.diazbayon@gmail.com>
2019-07-30 16:10:19 +0200

    Improve pipelines. Check if the SUT pod is ready

--- webapp/junit5-web-multiple-browsers-test/k8s/addSutPrefix.sh
+++ webapp/junit5-web-multiple-browsers-test/k8s/addSutPrefix.sh
@@ -1,3 +1,3 @@
 #/!bin/bash
 # Add sut prefix for the current execution 
-sed -i -e "s/name: demo-web-java-sut/name: ${ET_SUT_CONTAINER_NAME}-demo-web-java-sut/g" -e 's/sut_/sut-/g' webapp-deployment.yaml
+sed -i -e "s/name: demo-web-java-sut/name: ${ET_SUT_CONTAINER_NAME}-demo-web-java-sut/g" -e "s/io.elastest.service: demo-web-java-sut/io.elastest.service: ${ET_SUT_CONTAINER_NAME}-demo-web-java-sut/g" -e 's/sut_/sut-/g' webapp-deployment.yaml
