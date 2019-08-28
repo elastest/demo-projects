e53822a975bb855225fa21c6fd6d58a4d9f0c902
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2019-08-28 15:08:24 +0200

    Custommetrics

---
+++ custommetrics/src/test/java/com/custommetricstest/ElasTestBase.java
@@ -0,0 +1,23 @@
+package com.custommetricstest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ElasTestBase {
+
+    protected static final Logger logger = LoggerFactory.getLogger(ElasTestBase.class);
+
+    @BeforeEach
+    public void logStart(TestInfo testInfo) {
+        logger.info("##### Start test: " + testInfo.getTestMethod().get().getName());
+    }
+
+    @AfterEach
+    public void logEnd(TestInfo testInfo) {
+        logger.info("##### Finish test: " + testInfo.getTestMethod().get().getName());
+    }
+
+}
