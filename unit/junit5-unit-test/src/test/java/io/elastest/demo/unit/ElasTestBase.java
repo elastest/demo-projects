da9f21c7c3ac2c1f984cb2e08a7088bb24cfea68
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2019-09-11 09:09:48 +0200

    Add sleep to unit test

--- unit/junit5-unit-test/src/test/java/io/elastest/demo/unit/ElasTestBase.java
+++ unit/junit5-unit-test/src/test/java/io/elastest/demo/unit/ElasTestBase.java
@@ -1,5 +1,7 @@
 package io.elastest.demo.unit;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
@@ -12,12 +14,25 @@ public class ElasTestBase {
             .getLogger(ElasTestBase.class);
 
     @BeforeEach
-    public void logStart(TestInfo testInfo) {
-        logger.info("##### Start test: " + testInfo.getTestMethod().get().getName());
+    public void logStart(TestInfo testInfo) throws InterruptedException {
+        logger.info("##### Start test: "
+                + testInfo.getTestMethod().get().getName());
+        sleep(1);
     }
 
     @AfterEach
     public void logEnd(TestInfo testInfo) {
-        logger.info("##### Finish test: " + testInfo.getTestMethod().get().getName());
+        sleep(1);
+        logger.info("##### Finish test: "
+                + testInfo.getTestMethod().get().getName());
+    }
+
+    public void sleep(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
